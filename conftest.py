d1160745abd86df78d141b0eed7a3686f10d822e
Fenulechka <fenulechka@yandex.ru>
2025-04-14 16:32:54 +0200

    исправлено замечание ревьювера в файле conftest.py - убрана лишняя фикстура

--- conftest.py
+++ conftest.py
@@ -6,12 +6,3 @@ def collector():
     collector = BooksCollector()
     return collector
 
-@pytest.fixture
-def collection_genre(collector):
-    collector.add_new_book('Чудеса в решете')
-    collector.set_book_genre('Чудеса в решете', 'Комедии')
-    collector.add_new_book('Гостья из будущего')
-    collector.set_book_genre('Гостья из будущего', 'Фантастика')
-    collector.add_new_book('Паранормальные явления')
-    collector.set_book_genre('Паранормальные явления', 'Ужасы')
-    return collector.books_genre
